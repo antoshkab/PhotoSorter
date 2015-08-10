755240fa30fefc3321cbe08d19218e6cce607181
Anton <antoshkab@hotmail.com>
2015-08-10 17:04:27 +0300

    Create solution and project, implement some logic

---
+++ PhotoSorter/PhotoSorter/ActionCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PhotoSorter
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action<object> _action;
+        private readonly Predicate<object> _predicate;
+ 
+        public ActionCommand(Action<object> action) : this (action, null)
+        { }
+
+        public ActionCommand(Action<object> action, Predicate<object> predicate)
+        {
+            if (action == null)
+                throw new ArgumentNullException("action", "Action can't be null");
+            _action = action;
+            _predicate = predicate;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _predicate == null || _predicate(parameter);
+        }
+
+
+        public bool CanExecute()
+        {
+            return CanExecute(null);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
+
+        public void Execute()
+        {
+            Execute(null);
+        }
+    }
+}
---
+++ PhotoSorter/PhotoSorter/MainViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shell;
+
+namespace PhotoSorter
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _photoPath;
+
+        public string PhotoPath
+        {
+            get { return _photoPath; }
+            private set
+            {
+                _photoPath = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public bool UseSubFolder { get; set; }
+
+        public string CreateByMonth { get; set; }
+
+        private string _savePath;
+
+        public string SavePath
+        {
+            get { return _savePath; }
+            private set
+            {
+                _savePath = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        private TaskbarItemProgressState _progressState;
+
+        public TaskbarItemProgressState ProgressState
+        {
+            get { return _progressState; }
+            private set
+            {
+                _progressState = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> ProcessedFiles { get; private set; }
+
+        private readonly BackgroundWorker _backgroundWorker;
+
+        public ActionCommand SelectPhotoPathCommand
+        {
+            get { throw new NotImplementedException();}
+        }
+
+        public ActionCommand SelectSavePathCommand
+        {
+            get { throw new NotImplementedException();}
+        }
+
+        public ActionCommand StartProcessingCommand
+        {
+            get { throw new NotImplementedException();}
+        }
+
+        public ActionCommand StopProcessingCommand
+        {
+            get { throw new NotImplementedException();}
+        }
+
+        public MainViewModel()
+        {
+            _backgroundWorker = new BackgroundWorker();
+            _backgroundWorker.DoWork += BackgroundWorkerOnDoWork;
+            _backgroundWorker.ProgressChanged += BackgroundWorkerOnProgressChanged;
+            _backgroundWorker.RunWorkerCompleted += BackgroundWorkerOnRunWorkerCompleted;
+            ProcessedFiles = new ObservableCollection<string>();
+        }
+
+        private void BackgroundWorkerOnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BackgroundWorkerOnProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BackgroundWorkerOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
