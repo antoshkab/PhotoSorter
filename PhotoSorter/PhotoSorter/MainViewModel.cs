e0a1bfbdb5ac2000a870b66ecfe287089aef5958
Anton <antoshkab@hotmail.com>
2015-08-11 17:05:36 +0300

    TaskBarInfo set bindings from view model

--- PhotoSorter/PhotoSorter/MainViewModel.cs
+++ PhotoSorter/PhotoSorter/MainViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Configuration;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Forms;
