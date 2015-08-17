755addaf2c411f14eba701309187322093088b9b
Anton <antoshkab@hotmail.com>
2015-08-17 16:55:31 +0300

    Add some code

--- PhotoSorter/PhotoSorter/App.xaml.cs
+++ PhotoSorter/PhotoSorter/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,19 +13,46 @@ namespace PhotoSorter
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, IDisposable
     {
         private Mutex _mutex;
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            Assembly assembly = Assembly.GetExecutingAssembly();
             var isNewInstance = false;
-            _mutex = new Mutex(true, "PhotoSorter", out isNewInstance);
+            _mutex = new Mutex(true, string.Format("{0}/{1}", assembly.GetType().GUID, assembly.GetType().FullName),
+                out isNewInstance);
             if (!isNewInstance)
             {
+                _mutex = null;
                 Shutdown();
                 return;
             }
+
             base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Dispose();
+            base.OnExit(e);
+        }
+
+        private void Dispose(Boolean disposing)
+        {
+            if (disposing && (_mutex != null))
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Close();
+                _mutex = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
